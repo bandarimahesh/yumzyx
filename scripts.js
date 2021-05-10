6bfc2980aaf4a805cfe4fcadb498f4936e3f898f
bandarimahesh <68154163+bandarimahesh@users.noreply.github.com>
2021-05-10 17:32:27 +0530

    main website files uploaded

---
+++ scripts.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+  // typing text animation script
+  var typed = new Typed(".typing", {
+    strings: [
+      "Increase Loyal Customers.",
+      "Stop Paying Commissions.",
+      "Use Strategic Marketing.",
+      "Widen Your Profit Margins.",
+    ],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true,
+  });
+});
